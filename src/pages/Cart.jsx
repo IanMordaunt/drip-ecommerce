023f76aff90f47cc88b43f03f337539e246e4cea
Ian <new@news-MacBook-Pro.local>
2022-03-14 11:15:01 -0600

    update to cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,11 @@
-// import { Title } from "@material-ui/icons";
-// import { Info } from "@material-ui/icons";
+
+import { Add, Remove } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
 import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import Product from "../components/Product";
+// import Product from "../components/Product";
 
 const Container = styled.div`
 
@@ -56,10 +56,77 @@ const Info = styled.div`
 flex: 3;
 `;
 
+const Product = styled.div`
+display: flex;
+justify-content: space-between;
+
+`;
+
+const ProductDetail = styled.div`
+flex: 2;
+display: flex;
+
+`;
+
+const Image = styled.img`
+width: 200px;
+
+`;
+
+const Details = styled.div`
+padding: 20px;
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+
+`;
+
+const ProductName = styled.span`
+
+
+`;
+
+const ProductId = styled.span`
+
+
+`;
+
+const ProductQuantity = styled.span`
+
+
+`;
+
+const PriceDetail = styled.div`
+flex: 1;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+`;
+
+const ProductAmountContainer = styled.div`
+display: flex;
+align-content: center;
+margin-bottom: 20px;
+`;
+
+const ProductAmount = styled.div`
+font-size: 24px;
+margin: 5px;
+`;
+
+const ProductPrice = styled.div`
+font-size: 30px;
+font-weight: 200;
+`;
+
+
 const Summary = styled.div`
 flex: 1;
 
 `;
+
+
 
 
 
@@ -85,13 +152,37 @@ const Cart = () => {
                       <ProductDetail>
                           <Image src="https://iconikcoffee.com/wp-content/uploads/2020/11/singleRetail1.png" />
                       <Details>
-                          <ProductName><b>Product:</b>Ikonic - Mexico Washed</ProductName>
-                          <ProductId><b>ID:</b>93813718293</ProductId>
-                          <ProductRegion><b>ID:</b>93813718293</ProductId>
-                          <ProductId><b>ID:</b>93813718293</ProductId>
+                          <ProductName><b>Product: </b>Ikonic - Mexico Washed</ProductName>
+                          <ProductId><b>ID: </b>93813718293</ProductId>
+                          <ProductQuantity><b>Quantity: </b>12oz</ProductQuantity>
                       </Details>
                       </ProductDetail>
-                      <PriceDetail></PriceDetail>
+                      <PriceDetail>
+                          <ProductAmountContainer>
+                              <Add/>
+                              <ProductAmount>2</ProductAmount>
+                              <Remove/>
+                          </ProductAmountContainer>
+                          <ProductPrice>$ 20</ProductPrice>
+                      </PriceDetail>
+                  </Product>
+                  <Product>
+                      <ProductDetail>
+                          <Image src="https://iconikcoffee.com/wp-content/uploads/2020/11/singleRetail1.png" />
+                      <Details>
+                          <ProductName><b>Product: </b>Ikonic - Mexico Washed</ProductName>
+                          <ProductId><b>ID: </b>93813718293</ProductId>
+                          <ProductQuantity><b>Quantity: </b>12oz</ProductQuantity>
+                      </Details>
+                      </ProductDetail>
+                      <PriceDetail>
+                          <ProductAmountContainer>
+                              <Add/>
+                              <ProductAmount>2</ProductAmount>
+                              <Remove/>
+                          </ProductAmountContainer>
+                          <ProductPrice>$ 20</ProductPrice>
+                      </PriceDetail>
                   </Product>
               </Info>
               <Summary>summary</Summary>
