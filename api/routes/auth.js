6ee8b68cc6737f318f0c07f8744d4b33707d7fbc
Ian <new@news-MacBook-Pro.local>
2022-03-14 20:51:35 -0600

    added register and login routes in auth

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,14 +1,47 @@
 const router = require("express").Router();
-const User = require("../models/User")
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 // REGISTER
-router.post("/resister", (req,res)=>{
-    const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-       
-    })
-})
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
+  });
+  try {
+    const savedUser = await newUser.save();
+    res.status(200).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-module.exports = router
+// LOGIN
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json("Incorrect Credentials!");
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    originalPassword !== req.body.password &&
+      res.status(401).json("Incorrect Credentials!");
+
+    const { password, ...others } = user;
+
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
