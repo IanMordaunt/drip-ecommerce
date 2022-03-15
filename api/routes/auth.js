7bf11c8a5ed64c80c3b6123be5f51e40ecfd7c71
Ian <new@news-MacBook-Pro.local>
2022-03-14 21:55:41 -0600

    update to auth and updateUser in user.js

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 // REGISTER
 router.post("/register", async (req, res) => {
@@ -21,7 +22,6 @@ router.post("/register", async (req, res) => {
 });
 
 // LOGIN
-
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
@@ -36,9 +36,18 @@ router.post("/login", async (req, res) => {
     originalPassword !== req.body.password &&
       res.status(401).json("Incorrect Credentials!");
 
-    const { password, ...others } = user;
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
 
-    res.status(200).json(others);
+    const { password, ...others } = user._doc;
+
+    res.status(200).json({...others, accessToken});
   } catch (err) {
     res.status(500).json(err);
   }
