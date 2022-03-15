84bb0cd5588ee063f2259409ba2d23f092f4576d
Ian <new@news-MacBook-Pro.local>
2022-03-15 08:30:17 -0600

    update to products.js

--- api/routes/verifyToken.js
+++ api/routes/verifyToken.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers.token;
   if (authHeader) {
-      const token = authHeader.split(" ")[1];
+    const token = authHeader.split(" ")[1];
     jwt.verify(token, process.env.JWT_SEC, (err, user) => {
       if (err) res.status(403).json("Token is not valid!");
       req.user = user;
@@ -14,14 +14,28 @@ const verifyToken = (req, res, next) => {
   }
 };
 
-const verifyTokenAndAuthorization = (req,res, next)=>{
-    verifyToken(req,res,()=>{
-        if(req.user.id === req.params.id || req.user.isAdmin){
-        next()
-        }else{
-            res.status(403).json("Request not valid!")
-        }
-    });
-}
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("Request not valid!");
+    }
+  });
+};
 
-module.exports = { verifyToken, verifyTokenAndAuthorization };
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("Request not valid!");
+    }
+  });
+};
+
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+};
